112fbe3861c17bf6a3a5b156ccb4d7d0677be99d
gmdmgithub <gmika@interia.pl>
2019-07-10 13:21:06 +0200

    private/public key, RSA, sign options and payload

--- app.js
+++ app.js
@@ -9,31 +9,53 @@ dotenv.config({
 
 const app = express();
 
-const privateKey = fs.readFileSync('private.key', 'utf8');
+const privateKey = fs.readFileSync('./private.key', 'utf8');
+const publicKEY = fs.readFileSync('./public.key', 'utf8');
 const passPhrase = process.env.PASS_PHRASE;
+
+const signOptions = {
+    issuer: "My Company",
+    subject: "token.user@company.com",
+    expiresIn: "5m",
+    algorithm: "RS256"
+};
+
+let payload ={
+    someData1 : "Some string data",
+    someData2: true,
+    someData3: 23.5,
+    someData4: {
+        name: "Alex",
+        surname: "Springer"
+    }
+};
 
 app.get('/jwt', (req, res) => {
     // const token = jwt.sign({ "body": "This is my body" }, passPhrase, { algorithm: 'HS256'});
-    const token = jwt.sign({ "body": "This is my body" }, privateKey, { algorithm: 'HS256'});
+    const token = jwt.sign(payload, privateKey, signOptions);
     res.send(token);
 })
 
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hi there")
 })
 
-app.get("/protected",isAuthorized, (req,res)=>{
-    res.json({"message":"protected message"});
+app.get("/protected", isAuthorized, (req, res) => {
+    res.json({
+        "message": "protected message"
+    });
 })
 
-app.get("/open-message",(req,res)=>{
-    res.json({"message": "My message"});
+app.get("/open-message", (req, res) => {
+    res.json({
+        "message": "My message"
+    });
 });
 
 /**
  * Function isAuthorized checks if the request id authorized to access the endpoint
-* @param {http request} req 
+ * @param {http request} req 
  * @param {http response} res 
  * @param {pass} next 
  */
@@ -43,15 +65,19 @@ function isAuthorized(req, res, next) {
         // JWT using the split function
         console.log(req.headers);
         const token = req.headers.authorization.split(" ")[1];
-        
+
         // Here we validate that the JSON Web Token is valid and has been 
         // created using the same private pass phrase
-        jwt.verify(token, privateKey, { algorithm: "HS256" }, (err, user) => {
-            
+        jwt.verify(token, publicKEY, signOptions, (err, user) => {
+
             // if there has been an error...
-            if (err) {  
+            if (err) {
+                console.log(err);
+                
                 // shut them out!
-                res.status(500).json({ error: "Not Authorized" });
+                res.status(500).json({
+                    error: "Not Authorized"
+                });
                 throw new Error("Not Authorized");
             }
             // if the JWT is valid, allow them to hit
@@ -61,7 +87,9 @@ function isAuthorized(req, res, next) {
     } else {
         // No authorization header exists on the incoming
         // request, return not authorized and throw a new error 
-        res.status(500).json({ error: "Not Authorized" });
+        res.status(500).json({
+            error: "Not Authorized"
+        });
         throw new Error("Not Authorized");
     }
 }
